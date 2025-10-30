b19b037737924241c2b49194e3f8978e6fdd7f85
IShkuropiy <161413606+IShkuropiy@users.noreply.github.com>
2025-10-29 21:49:32 -0700

    adding new page - blog

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Hero/Home';
 import About from './pages/About';
 //import Contact from './pages/Contact';
-//import Blog from './pages/Blog';
+import Blog from './pages/Blog';
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   )
