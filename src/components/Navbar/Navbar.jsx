63621419e4dc5c6921b311913bfe2f3ae24e5695
IShkuropiy <161413606+IShkuropiy@users.noreply.github.com>
2025-11-02 21:04:03 -0800

    add navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
@@ -7,12 +8,13 @@ export default function Navbar() {
             <nav className="navbar-content">
                 <div className="logo">Iryna Shkuropii</div>
                 <ul className="nav-links">
-                    <li><a href="#hero">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contacts</a></li>
-                    <li><a href="#blog">Blog</a></li>
-                    
-                    <li><a href="#contact" className="btn-outline">Contact Me</a></li>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="/blog">Blog</NavLink>
+
+                    <li><a href="#contact" className="btn-outline">Contact Me</a>
+                    </li>
                 </ul>
             </nav>
         </div>
