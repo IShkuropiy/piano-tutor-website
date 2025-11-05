4a86797c3bfc92e12d87456913892a138a9d906c
IShkuropiy <161413606+IShkuropiy@users.noreply.github.com>
2025-11-05 12:20:43 -0800

    change home page layout

--- src/pages/Hero/Home.jsx
+++ src/pages/Hero/Home.jsx
@@ -1,22 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import hero from "/src/assets/hero.jpg";
 import "./Home.css";
-//import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
-
-        <>
-            <section id="hero" className="hero">
-
+        <main>
+            <section className="hero">
                 <div className="hero-content">
-                    <h1 className="hero-title">Iryna Shkuropii</h1>
-                    <h3 className="hero-subtitle">Classical Pianist and Tutor</h3>
-                    <p className="hero-text">
+                    <h1 className="title">Iryna Shkuropii</h1>
+                    <h3 className="subtitle">Classical Pianist and Tutor</h3>
+                    <p className="text">
                         Personalized piano lessons for every age, every stage
                     </p>
-                    <a href="#" className="btn">Contact Me</a>
+                    <li><NavLink to="/blog">Blog</NavLink></li>
+                </div>
+                 <div className="image">
+                     <img src={hero}/>
                 </div>
             </section>
-        </>
+        </main>
     );
 }
