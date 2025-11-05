5c7a6f66920d7741916882194bddb987c28d1eec
IShkuropiy <161413606+IShkuropiy@users.noreply.github.com>
2025-11-04 18:42:26 -0800

    add a new css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,16 +5,19 @@ import "./Navbar.css";
 export default function Navbar() {
     return (
         <div className="navbar">
-            <nav className="navbar-content">
+            <nav className="menu">
                 <div className="logo">Iryna Shkuropii</div>
                 <ul className="nav-links">
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/about">About</NavLink>
                     <NavLink to="/contact">Contact</NavLink>
                     <NavLink to="/blog">Blog</NavLink>
-
-                    <li><NavLink to="/contact" className="btn-outline">Contact Me</NavLink></li>
                 </ul>
+                <div className="contact-info">
+                    <span> Monday - Friday 9:00AM - 7:00PM</span>
+                    <span> Call Me (123) 456-7890</span>
+                </div>
+                 <li><NavLink to="/contact" className="btn-outline">Contact Me</NavLink></li>
             </nav>
         </div>
     );
