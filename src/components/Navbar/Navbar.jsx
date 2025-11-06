60552fa0c4dff5df583b516375aa3bb05f1d9daa
IShkuropiy <161413606+IShkuropiy@users.noreply.github.com>
2025-11-06 09:50:00 -0800

    delete extra link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,6 @@ export default function Navbar() {
                 <ul className="nav-links">
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/pricing">Pricing</NavLink></li>
                     <li><NavLink to="/blog">Blog</NavLink></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
