1f14511188b3f470d204e26188cebfffae287ffc
IShkuropiy <161413606+IShkuropiy@users.noreply.github.com>
2025-11-09 21:09:16 -0800

    changed home page and add a few assets to it

--- src/pages/Hero/Home.jsx
+++ src/pages/Hero/Home.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import hero from "/src/assets/hero.jpg";
-import "./Navbar.css";
+import "./Home.css";
+import hero from "/src/assets/hero.jpg";
+import kid from "/src/assets/kid.jpg";
+import reading from "/src/assets/reading.png";
+import brain from "/src/assets/brain.png";
+import music from "/src/assets/music.png";
 
 export default function Home() {
     return (
         <main>
-            <section className="hero">
-                <div className="hero-content">
-                    <h1 className="title">Individual Piano Lessons: Personalized Learning. Real Results.</h1>
-                    <p className="text">
-                        Whether you're starting from scratch or looking to revisit the skills you mastered in music school, private piano lessons are the perfect place to begin (or continue!) your musical journey.
-                    </p>
-                    <li><NavLink to="/about">Learn more</NavLink></li>
-                </div>
-                <div className="image">
-                    <img src={hero} />
-                </div>
-            </section>
+        <section className="hero">
+        <div className="hero-content">
+          <h1 className="title">Individual Piano Lessons: Personalized Learning. Real Results.</h1>
+          <p className="text">
+            Whether you're starting from scratch or looking to revisit the skills you mastered in music school, private piano lessons are the perfect place to begin (or continue!) your musical journey.
+          </p>
+          <Link to="/about" className="btn">Learn more</Link>
+        </div>
+
+        <div className="image">
+          <img src={hero} />
+        </div>
+      </section>
         </main>
     );
 }
